91620eb2de120a322da735c2ee80e4e6dd2d00f5
esp32-urobot <urobot2011@gmail.com>
2023-02-17 09:32:55 +0900

    Update changeTheme.js

--- MartyBot/colorugrey/changeTheme.js
+++ MartyBot/colorugrey/changeTheme.js
@@ -21,7 +21,7 @@ $("colourchange").html(`
       </div>
 `);
 $("#colourchangebtn").click(function () {
-  var selectmenu = $("#boardthemeselect").val;
+  var selectmenu = $("#boardthemeselect").val();
   var blcsquare1 = document.querySelectorAll(".black-3c85d");
         var whtsquare1 = document.querySelectorAll(".white-1e1d7");
         for (i = 0; i < whtsquare1.length; i++) {
